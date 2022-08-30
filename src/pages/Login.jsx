f8632271122805aa48cdc303b8384d0f603d1a88
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-08-30 18:47:08 -0300

    requisito 2 feito

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Login extends React.Component {
+  state = {
+    login: '',
+    disable: true,
+    loading: false,
+  };
+
+  handleChange = (event) => {
+    const isValid = 3;
+    this.setState({ login: event.target.value,
+      disable: event.target.value.length < isValid,
+    });
+  };
+
+  isLoading = async () => {
+    const { login } = this.state;
+    const { history } = this.props;
+    console.log(login);
+    this.setState({ loading: true });
+    await createUser({ name: login });
+    history.push('search');
+  };
+
   render() {
-    return (<div data-testid="page-login" />);
+    const { login, disable, loading } = this.state;
+    return (
+      <div data-testid="page-login">
+        {loading ? <Carregando /> : (
+          <form>
+            <label htmlFor="login">
+              <input
+                type="text"
+                name="login"
+                onChange={ this.handleChange }
+                value={ login }
+                id="login"
+                data-testid="login-name-input"
+              />
+            </label>
+            <button
+              type="button"
+              disabled={ disable }
+              data-testid="login-submit-button"
+              onClick={ () => this.isLoading() }
+            >
+              Entrar
+            </button>
+          </form>
+        )}
+      </div>
+    );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
