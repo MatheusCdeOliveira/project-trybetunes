485697e973ca40eab09ff2038853adbeeff11681
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-08-31 17:26:38 -0300

    requisito 5 feito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,42 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artistName: '',
+    enabled: true,
+  };
+
+  handleInputChange = (event) => {
+    const MIN_CHARACTER = 2;
+    const { value } = event.target;
+    this.setState({ artistName: value, enabled: (value.length < MIN_CHARACTER) });
+  };
+
   render() {
+    const { artistName, enabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search">
+            <input
+              data-testid="search-artist-input"
+              value={ artistName }
+              onChange={ this.handleInputChange }
+              type="text"
+              name="search"
+              id=""
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ enabled }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
